eeefce52e31351ab50e988cf3a74e4a612067438
Mike Roylance <roylance.michael@gmail.com>
2016-11-14 13:28:39 -0700

    updated dependencies

--- python/properties.py
+++ python/properties.py
@@ -1,10 +1,10 @@
-#generated from yaclib on Thu Nov 10 15:33:22 MST 2016
-#Thu Nov 10 15:33:22 MST 2016
+#generated from yaclib on Mon Nov 14 13:17:26 MST 2016
+#Mon Nov 14 13:17:26 MST 2016
 major="0"
 name="api"
 githubUrl="https\://github.com/roylanceMichael/yaorm.git"
 author="roylance.michael@gmail.com"
 description="models to interface with the org.roylance.yaorm.api system"
 license="MIT"
-minor="146"
+minor="150"
 group="org.roylance.yaorm"
