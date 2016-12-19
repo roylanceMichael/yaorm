a3fa39337ba69915934264af90394b5f27729694
Mike Roylance <roylance.michael@gmail.com>
2016-12-19 10:43:28 -0700

    merged changes

--- javascript/IYaormMainService.ts
+++
@@ -1,9 +0,0 @@
-// THIS FILE WAS AUTO-GENERATED. DO NOT ALTER!
-export interface IYaormMainService {
-	get_schemas(request: org.roylance.yaorm.UIYaormRequest, onSuccess:(response: org.roylance.yaorm.UIYaormResponse)=>void, onError:(response:any)=>void)
-	get_tables(request: org.roylance.yaorm.UIYaormRequest, onSuccess:(response: org.roylance.yaorm.UIYaormResponse)=>void, onError:(response:any)=>void)
-	get_table_definition(request: org.roylance.yaorm.UIYaormRequest, onSuccess:(response: org.roylance.yaorm.UIYaormResponse)=>void, onError:(response:any)=>void)
-	get_table_definitions(request: org.roylance.yaorm.UIYaormRequest, onSuccess:(response: org.roylance.yaorm.UIYaormResponse)=>void, onError:(response:any)=>void)
-	get_record_count(request: org.roylance.yaorm.UIYaormRequest, onSuccess:(response: org.roylance.yaorm.UIYaormResponse)=>void, onError:(response:any)=>void)
-	get_records(request: org.roylance.yaorm.UIYaormRequest, onSuccess:(response: org.roylance.yaorm.UIYaormResponse)=>void, onError:(response:any)=>void)
-}
